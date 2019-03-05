85ac8c998978036fbd972d7c2ea94f1b7db6900e
Bowen Yu <bowen@barcelona-umh.cs.umn.edu>
2019-03-04 21:40:58 -0600

    Adding all pairwise plots

--- code/accuracy.py
+++ code/accuracy.py
@@ -51,9 +51,11 @@ class AccuracyTradeOffs:
 
         it = 0
         # Threshold splits between [0, 1]: (1) 0, 1, 21: 0.05; (2) 0, 1, 41: 0.025
-        thresholds = np.linspace(0, 1, 41, endpoint=True)
-        dict_rates_fp = {}
-        dict_rates_fn = {}
+        thresholds = np.linspace(0, 1, 101, endpoint=True)
+        dict_rates_fp_damaging = {}
+        dict_rates_fn_damaging = {}
+        dict_rates_fp_badfaith = {}
+        dict_rates_fn_badfaith = {}
         dict_precision = {}
         dict_accuracy = {}
         dict_sensitivity = {}  # recall
@@ -64,8 +66,11 @@ class AccuracyTradeOffs:
 
         for threshold in thresholds:
             threshold = str(round(threshold, self.decimal))
-            dict_rates_fp[threshold] = 0
-            dict_rates_fn[threshold] = 0
+            dict_rates_fp_damaging[threshold] = 0
+            dict_rates_fn_damaging[threshold] = 0
+            dict_rates_fp_badfaith[threshold] = 0
+            dict_rates_fn_badfaith[threshold] = 0
+
             dict_precision[threshold] = 0
             dict_accuracy[threshold] = 0
             dict_sensitivity[threshold] = 0
@@ -75,41 +80,39 @@ class AccuracyTradeOffs:
             it += 1
             print("Working on Iteration {} ..".format(it))
 
-            if self.label_type == 'quality':
-                data_y = self.data_y_damaging
-            elif self.label_type == 'intention':
-                data_y = self.data_y_badfaith
-            else:
-                print("Invalid prediction label ..")
-                return
-
-            # todo: include editor features here?
             X_train, X_test = self.data_x[train_idx], self.data_x[test_idx]
-            Y_train, Y_test = data_y[train_idx], data_y[test_idx]
+            y_train_damaging, y_test_damaging = self.data_y_damaging[train_idx], self.data_y_damaging[test_idx]
+            y_train_badfaith, y_test_badfaith = self.data_y_badfaith[train_idx], self.data_y_badfaith[test_idx]
 
             # clf = LogisticRegression(max_iter=500, penalty='l1', C=1)  # default P>0.5
             # clf = AdaBoostClassifier()
             # clf = AdaBoostClassifier(learning_rate=0.01, n_estimators=100,
             #                          base_estimator=DecisionTreeClassifier(max_depth=4, max_features="sqrt"))
-            # clf = GradientBoostingClassifier(learning_rate=0.01, max_depth=7, max_features="sqrt", n_estimators=700)
+            clf = GradientBoostingClassifier(learning_rate=0.01, max_depth=7, max_features="sqrt", n_estimators=700)
             # clf = RandomForestClassifier(n_estimators=500, max_features="sqrt", max_depth=7)
-            clf = MLPClassifier(learning_rate='adaptive', max_iter=500, hidden_layer_sizes=100)
+            # clf = MLPClassifier(learning_rate='adaptive', max_iter=500, hidden_layer_sizes=100)
 
-            clf.fit(X_train, Y_train)
+            clf.fit(X_train, y_train_damaging)
+            # clf.fit(X_train, y_train_badfaith)
             Y_pred_prob = clf.predict_proba(X_test)
+
             Y_pred_score = []
             for neg, pos in Y_pred_prob:
                 Y_pred_score.append(pos)
 
-            list_roc_auc.append(roc_auc_score(y_true=Y_test, y_score=Y_pred_score))
-            list_pr_auc.append(average_precision_score(y_true=Y_test, y_score=Y_pred_score))
+            list_roc_auc.append(roc_auc_score(y_true=y_test_damaging, y_score=Y_pred_score))
+            list_pr_auc.append(average_precision_score(y_true=y_test_damaging, y_score=Y_pred_score))
 
             for threshold in thresholds:
                 list_Y_pred = []
+
                 for score in Y_pred_prob:
                     list_Y_pred.append(1 if score[1] >= threshold else 0)
 
-                tn, fp, fn, tp = confusion_matrix(y_true=Y_test, y_pred=list_Y_pred).ravel()
+                threshold = str(round(threshold, self.decimal))
+
+                # Predicting damaging edits
+                tn, fp, fn, tp = confusion_matrix(y_true=y_test_damaging, y_pred=list_Y_pred).ravel()
                 rate_fp = fp / (fp + tn)
                 rate_tp = tp / (tp + fn)  # sensitivity/recall/true positive rates
                 rate_fn = fn / (fn + tp)
@@ -117,28 +120,44 @@ class AccuracyTradeOffs:
                 precision = 0 if tp + fp == 0 else tp / (tp + fp)
                 accuracy = (tp + tn) / (tp + tn + fp + fn)
 
-                threshold = str(round(threshold, self.decimal))
-                dict_rates_fp[threshold] += rate_fp
-                dict_rates_fn[threshold] += rate_fn
+                dict_rates_fp_damaging[threshold] += rate_fp
+                dict_rates_fn_damaging[threshold] += rate_fn
 
                 dict_precision[threshold] += precision
                 dict_sensitivity[threshold] += rate_tp
                 dict_accuracy[threshold] += accuracy
                 dict_specificity[threshold] += rate_tn
 
+                # Predicting bad-faith edits
+                tn, fp, fn, tp = confusion_matrix(y_true=y_test_badfaith, y_pred=list_Y_pred).ravel()
+                rate_fp = fp / (fp + tn)
+                rate_tp = tp / (tp + fn)  # sensitivity/recall/true positive rates
+                rate_fn = fn / (fn + tp)
+                rate_tn = tn / (tn + fp)  # specificity/true negative rates
+                precision = 0 if tp + fp == 0 else tp / (tp + fp)
+                accuracy = (tp + tn) / (tp + tn + fp + fn)
+
+                # threshold = str(round(threshold, self.decimal))
+                dict_rates_fp_badfaith[threshold] += rate_fp
+                dict_rates_fn_badfaith[threshold] += rate_fn
+
         f_output = open("{}_{}.csv".format(self.plot_output, self.label_type), 'w')
         print("Threshold  Precision  Sensitivity/Recall  Specificity  Accuracy  False Positive  False Negative")
         for threshold in thresholds:
             threshold = str(round(threshold, self.decimal))
-            dict_rates_fp[threshold] /= self.n_folds
-            dict_rates_fn[threshold] /= self.n_folds
+            dict_rates_fp_damaging[threshold] /= self.n_folds
+            dict_rates_fn_damaging[threshold] /= self.n_folds
+            dict_rates_fp_badfaith[threshold] /= self.n_folds
+            dict_rates_fn_badfaith[threshold] /= self.n_folds
             # print("{}, \t{:.5f}, \t{:.5f}".format(threshold,
             #                                       dict_rates_fp[threshold],
             #                                       dict_rates_fn[threshold]))
 
-            print("{},{:.5f},{:.5f}".format(threshold,
-                                            dict_rates_fp[threshold],
-                                            dict_rates_fn[threshold]), file=f_output)
+            print("{},{:.5f},{:.5f},{:.5f},{:.5f}".format(threshold,
+                                                          dict_rates_fp_damaging[threshold],
+                                                          dict_rates_fn_damaging[threshold],
+                                                          dict_rates_fp_badfaith[threshold],
+                                                          dict_rates_fn_badfaith[threshold]), file=f_output)
 
             dict_precision[threshold] /= self.n_folds
             dict_sensitivity[threshold] /= self.n_folds  # recall
@@ -150,8 +169,8 @@ class AccuracyTradeOffs:
                                                                                   dict_sensitivity[threshold],
                                                                                   dict_specificity[threshold],
                                                                                   dict_accuracy[threshold],
-                                                                                  dict_rates_fp[threshold],
-                                                                                  dict_rates_fn[threshold]))
+                                                                                  dict_rates_fp_damaging[threshold],
+                                                                                  dict_rates_fn_damaging[threshold]))
 
         print("ROC_AUC {}\tPR_AUC {}".format(sum(list_roc_auc) / len(list_roc_auc),
                                              sum(list_pr_auc) / len(list_pr_auc)))
@@ -183,12 +202,57 @@ class AccuracyTradeOffs:
         plt.plot(x, y, marker='o')
         plt.show()
 
+    def plot_all_pairs(self):
+
+        l_fp_damaging = []
+        l_fn_damaging = []
+        l_fp_badfaith = []
+        l_fn_badfaith = []
+
+        for line in open("{}.csv".format(self.plot_output), 'r'):
+            threshold, fp_damaging, fn_damaging, fp_badfaith, fn_badfaith = line.strip().split(',')
+            l_fp_damaging.append(float(fp_damaging))
+            l_fn_damaging.append(float(fn_damaging))
+            l_fp_badfaith.append(float(fp_badfaith))
+            l_fn_badfaith.append(float(fn_badfaith))
+
+        # FP_damaging False-positive rate of predicting damaging edits: save patrollers’ efforts
+        # FN_damaging false-negative rate of predicting damaging edits: quality control
+        # FP_badfaith False-positive rate of predicting bad faith edits: motivation protection
+        # FN_badfaith False-negative rate of predicting bad faith edits: counter-vandalism
+
+        # Six polar axes
+        f, axarr = plt.subplots(2, 3)
+
+        axarr[0, 0].plot(l_fp_badfaith, l_fn_badfaith, marker='o')
+        axarr[0, 0].set_title('Motivation Protection (x) V.S. \nCounter-Vandalism (y)')
+
+        axarr[0, 1].plot(l_fp_damaging, l_fn_damaging, marker='o')
+        axarr[0, 1].set_title('Save Patrollers’ Efforts (x) V.S. \nQuality Control (y)')
+
+        axarr[0, 2].plot(l_fp_damaging, l_fn_badfaith, marker='o')
+        axarr[0, 2].set_title('Save Patrollers’ Efforts (x) V.S. \nCounter-Vandalism (y)')
+
+        axarr[1, 0].plot(l_fp_badfaith, l_fn_damaging, marker='o')
+        axarr[1, 0].set_title('Motivation Protection (x) V.S. \nQuality Control (y)')
+
+        axarr[1, 1].plot(l_fp_badfaith, l_fp_damaging, marker='o')
+        axarr[1, 1].set_title('Motivation Protection (x) V.S. \nSave Patrollers’ Efforts (y)')
+
+        axarr[1, 2].plot(l_fn_badfaith, l_fn_damaging, marker='o')
+        axarr[1, 2].set_title('Counter-Vandalism (x) V.S. \nQuality Control (y)')
+        # Fine-tune figure; make subplots farther from each other.
+        f.subplots_adjust(hspace=0.3)
+
+        plt.show()
+
 
 def main():
     runner = AccuracyTradeOffs()
     runner.load_data()
     runner.run_cross_validation()
-    runner.plot_charts()
+    # runner.plot_charts()
+    runner.plot_all_pairs()
 
 
 if __name__ == '__main__':
